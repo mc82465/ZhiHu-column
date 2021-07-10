064c0211a5c8af20ed83772f464757b91f23b4b9
xmmc <1014198200@qq.com>
2021-07-10 22:53:53 +0800

    dropdown下拉菜单组件及自定义函数

---
+++ zheye/src/components/Dropdown .vue
@@ -0,0 +1,56 @@
+<template>
+<div class="dropdown" ref="dropdownRef">
+  <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">
+    {{title}}
+  </a>
+  <ul class="dropdown-menu" :style="{display: 'block'}" v-if="isOpen">
+      <slot></slot>
+  </ul>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, watch } from 'vue'
+import useClickOutside from '../hooks/userClickOutside'
+export default defineComponent({
+  name: 'Dropdown',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup () {
+    const isOpen = ref(false)
+    const dropdownRef = ref<null | HTMLElement>(null)
+    const toggleOpen = () => {
+      isOpen.value = !isOpen.value
+    }
+    /* const handler = (e: MouseEvent) => {
+      if (dropdownRef.value) {
+        if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
+          isOpen.value = false
+        }
+      }
+    }
+    onMounted(() => {
+      document.addEventListener('click', handler)
+    })
+    onUnmounted(() => {
+      document.removeEventListener('click', handler)
+    }) */
+    const isClickOutside = useClickOutside(dropdownRef)
+    // 监听isClickOutside变化
+    watch(isClickOutside, () => {
+      if (isOpen.value && isClickOutside.value) {
+        isOpen.value = false
+      }
+    })
+    return {
+      isOpen,
+      toggleOpen,
+      dropdownRef
+    }
+  }
+})
+</script>
---
+++ zheye/src/hooks/userClickOutside.ts
@@ -0,0 +1,24 @@
+import { ref, onMounted, onUnmounted, Ref } from 'vue'
+// eslint-disable-next-line
+const useClickOutside = (elementRef: Ref<null | HTMLElement>) => {
+  const isClickOutside = ref(false)
+  const handler = (e: MouseEvent) => {
+    if (elementRef.value) {
+      if (elementRef.value.contains(e.target as HTMLElement)) {
+        isClickOutside.value = false
+      } else {
+        isClickOutside.value = true
+      }
+      console.log(isClickOutside.value)
+    }
+  }
+  onMounted(() => {
+    document.addEventListener('click', handler)
+  })
+  onUnmounted(() => {
+    document.removeEventListener('click', handler)
+  })
+  return isClickOutside
+}
+
+export default useClickOutside
