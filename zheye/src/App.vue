ba46cf3e51f5abc3d0cec6923dae8ce16f879f59
xmmc <1014198200@qq.com>
2021-07-11 10:47:52 +0800

    validate验证组件，及抽象验证规则

--- zheye/src/App.vue
+++ zheye/src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <div class="container">
         <global-header :user="currentUser"></global-header>
-    <column-list :list="list"></column-list>
+        <!-- <column-list :list="list"></column-list> -->
+        <form action="">
+          <div class="mb-3">
+            <label class="form-label">邮箱地址</label>
+            <validate-input :rules="emailRules"></validate-input>
+          </div>
+
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+            <input
+              type="text" class="form-control" id="exampleInputEmail1"
+              v-model="emailRef.val" @blur="vaildateEmail"
+            >
+            <div class="form-text" v-if="emailRef.error">{{emailRef.message}}</div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">密码</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+        </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
-
+import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
 const currentUser: UserProps = {
   isLogin: true,
   name: 'xm'
 }
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -44,13 +64,35 @@ const testData: ColumnProps[] = [
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
-    GlobalHeader
+    // ColumnList,
+    GlobalHeader,
+    ValidateInput
   },
   setup () {
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ]
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    const vaildateEmail = () => {
+      if (emailRef.val.trim() === '') {
+        emailRef.error = true
+        emailRef.message = 'can nopt be empty'
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true
+        emailRef.message = 'should be valid email'
+      }
+    }
     return {
       list: testData,
-      currentUser
+      currentUser,
+      emailRef,
+      vaildateEmail,
+      emailRules
     }
   }
 })
