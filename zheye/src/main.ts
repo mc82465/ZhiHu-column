6e0ac28d2e79c2b42e2f3717e0862f9b1ef6aa6b
xmmc <1014198200@qq.com>
2021-07-15 21:06:53 +0800

    专栏columndetail与专栏下列表postlist

--- zheye/src/main.ts
+++ zheye/src/main.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import Home from './views/Home.vue'
 import Login from './views/login.vue'
+import ColumnDetail from './views/ColumnDetail.vue'
 
 const routerHistory = createWebHashHistory()
 const router = createRouter({
@@ -17,6 +18,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/column/:id',
+      name: 'column',
+      component: ColumnDetail
     }
   ]
 })
